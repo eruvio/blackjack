c9fe0246ffea45e9176f1dcf41a36e9857d6a6fe
Eric Ruvio <eruvio90@gmail.com>
2014-02-20 22:47:18 -0500

    initial commit with images

---
+++ Blackjack/Blackjack/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Blackjack
+//
+//  Created by Eric Ruvio on 2/20/14.
+//  Copyright (c) 2014 Eric Ruvio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Blackjack/Blackjack/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Blackjack
+//
+//  Created by Eric Ruvio on 2/20/14.
+//  Copyright (c) 2014 Eric Ruvio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
