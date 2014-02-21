e8f98da9376d865fec46d1aeb4d2094a956aa0a6
Eric Ruvio <eruvio90@gmail.com>
2014-02-20 23:13:20 -0500

    added connections to cards+buttons

--- Blackjack/Blackjack/ViewController.h
+++ Blackjack/Blackjack/ViewController.h
@@ -10,4 +10,20 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIImageView *dealerCard1;
+@property (weak, nonatomic) IBOutlet UIImageView *dealerCard2;
+@property (weak, nonatomic) IBOutlet UIImageView *dealerCard3;
+@property (weak, nonatomic) IBOutlet UIImageView *dealerCard4;
+@property (weak, nonatomic) IBOutlet UIImageView *dealerCard5;
+
+@property (weak, nonatomic) IBOutlet UIImageView *playerCard1;
+@property (weak, nonatomic) IBOutlet UIImageView *playerCard2;
+@property (weak, nonatomic) IBOutlet UIImageView *playerCard3;
+@property (weak, nonatomic) IBOutlet UIImageView *playerCard4;
+@property (weak, nonatomic) IBOutlet UIImageView *playerCard5;
+
+@property (weak, nonatomic) IBOutlet UIButton *standButton;
+@property (weak, nonatomic) IBOutlet UILabel *hitButton;
+
+
 @end
